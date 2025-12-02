777b8436fb84b1ced44a02d3286fdd30b84f6cc3
Banana <x.boogieboo.x@gmail.com>
2025-12-02 16:18:15 +0200

    fix-task-1-3

--- js/task-3.js
+++ js/task-3.js
@@ -1,7 +1,7 @@
 function filterArray(numbers, value) {
     let massive = [];
 
-    for (let i = 0; i <= numbers.length; i++) {
+    for (let i = 0; i < numbers.length; i++) {
         if (numbers[i] <= value) {
         }
         else {
